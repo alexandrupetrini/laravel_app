c8e76de8f8828de6be30caa625096e9104cceb65
alexandrupetrini <alexandru.c.petrini@gmail.com>
2020-10-05 01:34:53 +0300

    detached

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ if [ -n "$(docker container ls -f name=$nginx_name -q)" ]; then docker container
 if [ -n "$(docker container ls -f name=$app_name -q)" ]; then docker container rm -f $app_name; fi
 if [ -z "$(docker network ls -f name=$networkName -q)" ]; then docker network create --driver bridge $networkName; fi
 
-docker container run --name=$app_name --network $networkName --restart always $app_prod
-docker container run --name=$nginx_name --network $networkName -p 80:8181 -p 443:8143 --restart always $nginx_prod
+docker container run -d --name=$app_name --network $networkName --restart always $app_prod
+docker container run -d --name=$nginx_name --network $networkName -p 80:8181 -p 443:8143 --restart always $nginx_prod
